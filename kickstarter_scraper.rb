7621d9d2ff0e105fba0f3b639377a619c3c23117
Trevis McGill <trevismcgill25@gmail.com>
2020-08-12 17:56:14 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,10 +3,18 @@ require "nokogiri"
 require "pry"
 
 def create_project_hash
-  # write your code here
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+
+  projects = {}
+
+  # Iterate through the projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+
+  # return the projects hash
+  projects
 end
 
 projects: kickstarter.css("li.project.grid_4")
